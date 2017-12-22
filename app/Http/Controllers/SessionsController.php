98ce9f55bc155e92e619af5e7e6c2e0a34143088
tonyinchn <tonyinchn@foxmail.com>
2017-12-22 08:22:33 +0000

    Add account activations

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,8 +9,8 @@ use Illuminate\Support\Facades\Auth;
 class SessionsController extends Controller {
     //
     public function __construct() {
-        $this->middleware('guest',[
-           'only' => ['create']
+        $this->middleware('guest', [
+            'only' => ['create']
         ]);
     }
 
@@ -23,9 +23,16 @@ class SessionsController extends Controller {
             'email' => 'required|email|max:255',
             'password' => 'required'
         ]);
-        if (Auth::attempt($credentials,$request->has('remember'))) {
-            session()->flash('success', '欢迎回来');
-            return redirect()->intended(route('users.show', [Auth::user()]));
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来');
+                return redirect()->intended(route('users.show', [Auth::user()]));
+            } else {
+                Auth::logout();
+                session()->flash('warning','您的账号未激活,请检查邮箱中的注册右键进行激活.');
+                return redirect('/');
+            }
+
         } else {
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
@@ -35,7 +42,7 @@ class SessionsController extends Controller {
 
     public function destroy() {
         Auth::logout();
-        session()->flash('success','您已成功推出');
+        session()->flash('success', '您已成功推出');
         return redirect('login');
     }
 }
